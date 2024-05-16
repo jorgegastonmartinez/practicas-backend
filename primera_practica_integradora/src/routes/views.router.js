2d889b8ddb8b91346e144570cf4638a4d0c949d3
jorgegastonmartinez <jorge_gmartinez@hotmail.com>
2024-05-16 17:15:13 -0300

    casi commit

--- primera_practica_integradora/src/routes/views.router.js
+++ primera_practica_integradora/src/routes/views.router.js
@@ -1,9 +1,16 @@
 import express from "express";
+import messageModel from "../models/messages.model.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index", {});
+router.get("/", async (req, res) => {
+  try {
+    const mensajes = await messageModel.find().lean();
+    res.render("index", {mensajes});
+  } catch (error) {
+    console.log("error al mostrar los mensajes", error);
+  }
+  
 });
 
 export default router;
