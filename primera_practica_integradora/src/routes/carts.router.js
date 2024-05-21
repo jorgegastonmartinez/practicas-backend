a845326929feef703775b76653429da914aa323e
jorgegastonmartinez <jorge_gmartinez@hotmail.com>
2024-05-21 00:46:16 -0300

    creacion de carts

--- primera_practica_integradora/src/routes/carts.router.js
+++ primera_practica_integradora/src/routes/carts.router.js
@@ -4,37 +4,27 @@ import productModel from "../dao/models/product.model.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.post("/api/carts", async (req, res) => {
     try {
-        let carts = await cartModel.find();
-        res.send({ result: "success", payload: carts });
-    } catch (error) {
-        console.error("Error al obtener el carrito", error);
-        res.status(500).json({ error: "Error al obtener el carrito" });
-    }
-})
+        const newCart = new cartModel({
+          products: [],
+        });
 
-// router.get("/:cid", async (req, res) => {
-//     try {
-//         let {cid} = req.params;
-//         let cart = await cartModel.findById(cid);
-        
-//         if (!cart) {
-//             return res.status(400).send({error: "Carrito no encontrado"})
-//         }
-//         res.send({ result: "success", payload: cart });
-//     } catch (error) {
-//         console.error("Error al obtener el carrito");
-//         res.status(500).json({ error: "Error al obtener el carrito" });
-//     }
-// })
+        await newCart.save();
+
+        res.json({ message: "Carrito creado correctamente", cart: newCart });
+    } catch (error) {
+        console.error("Error al crear el carrito:", error);
+        res.status(500).json({ error: "Ocurrió un error al crear el carrito" });
+    }
+});
 
 router.get("/:cid", async (req, res) => {
     try {
-        let {cid} = req.params;
+        let { cid } = req.params;
 
         if (!mongoose.Types.ObjectId.isValid(cid)) {
-            return res.status(400).json({ error: "ID de carrito no válido" });
+          return res.status(400).json({ error: "ID de carrito no válido" });
         }
 
         let cart = await cartModel.findById(cid);
@@ -58,6 +48,7 @@ router.get("/:cid", async (req, res) => {
           ...cart.toObject(),
           products,
         };
+
         res.send({ result: "success", payload: cartWithProducts });
 
     } catch (error) {
@@ -66,6 +57,44 @@ router.get("/:cid", async (req, res) => {
     }
 });
 
+router.post("/:cid/product/:pid", async (req, res) => {
+    try {
+        let { cid } = req.params;
+        let { pid } = req.params;
 
+        if (!mongoose.Types.ObjectId.isValid(cid) || !mongoose.Types.ObjectId.isValid(pid)) {
+            return res.status(400).json({ error: "ID carrito o producto no válido" });
+        }
+
+        let cart = await cartModel.findById(cid);
+        if (!cart) {
+            return res.status(404).json({ error: "Carrito no encontrado" });
+        }
+
+        let product = await productModel.findById(pid);
+        if (!product) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+
+        const existsProductIndex = cart.products.findIndex(item => item.product.toString() === pid);
+
+        if (existsProductIndex !== -1) {
+            cart.products[existsProductIndex].quantity++;
+        } else {
+            const productToAdd = {
+                product: mongoose.Types.ObjectId(pid),
+                quantity: 1
+            };
+            cart.products.push(productToAdd);
+        }
+
+        await cart.save();
+
+        res.json({ message: "Producto añadido al carrito correctamente", cart });
+    } catch (error) {
+        console.error("Error al agregar producto al carrito", error);
+        res.status(500).json({ error: "Ocurrió un error al agregar producto al carrito" });
+    }
+});
 
 export default router;
